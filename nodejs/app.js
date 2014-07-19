31f2d0de092ca50c5bc4352fe1ef6f39810637a2
David Perlaza <superlaza7@gmail.com>
2014-07-19 11:51:43 -0400

    Update app.js
    Reverted to old app in order to push newest changes to alternative branch

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,16 +1,21 @@
 //modules used
-var fs = require('fs');//file system
-var http = require('http');//server
+var fs = require('fs');
+var http = require('http');
 var url = require('url');
 var path =require('path');
-var mime = require('mime');
+var mime = require('mime');//downloaded with npm
 var qs = require('querystring');
-var exec = require('child_process').spawn;
-var formidable = require('formidable');//form handling
+var exec = require('child_process').spawn
+
+//for formidable
+var formidable = require('formidable');
 var util = require('util');
-var socket = require('socket.io');//socket.io fast message relay
-var events = require('events');//custom events firing
-var express = require('express');
+
+//socket.io
+var socket = require('socket.io');
+
+//custom events firing
+var events = require('events');
 
 //handle transport of data
 var dataTransport = function(){this.data = undefined};
@@ -18,149 +23,174 @@ var dataTransport = function(){this.data = undefined};
 dataTransport.prototype = new events.EventEmitter;
 var dt = new dataTransport();
 
-var app = express();
+//read main file, when done launch server
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        console.log(err);
+    }
 
-//main route
-app.get('/', function(req, res){
-    fs.readFile('./index.html', function (err, html) {
-        if (err) {
-            console.log(err);
-        }
-        res.set('Content-Type', 'text/html');
-        res.send(html);
-    });
-});
+    var app = http.createServer(function(req, res) {
+        //this method should be in a handler pointed to by action
+        //consider returning from createServer function after POST req
+        if (req.method == 'POST') {
 
-//any other request, find file and server
-app.get('*', function(req, res){
-    var uri = url.parse(req.url).pathname;
-    fs.readFile('./'+uri, function(err, data){
-        if (err){
-            //file can't be found
-            console.log('Error loading '+uri);
-            res.send(404);
-            return;
-        }
+            // parse a file upload
+            var form = new formidable.IncomingForm();
 
-        res.set('Content-Type', mime.lookup(uri));
-        res.send(data);
-    });
-});
+            //set params
+            form.uploadDir = '../img';
+            form.keepExtensions = true;
 
-//post handling
-//TODO: modify action in html to use custom router
-app.post('/upload', function(req, res){
-    console.log('we get to post');
-    //TODO: ALL POSSIBLE STATE EVOLUTIONS SHOULD BE ACCOUNTED FOR
-    // parse a file upload
-    var form = new formidable.IncomingForm();
+            //register listener on file reception event
+            //in order to change file path to one with file's original name
+            //default generates random name for storage
+            form.on('fileBegin', function(name, file) {
+                //save to current directory
+                //need to specify file path at moment of file discovery
+                //otherwise it will be ignored when the writestream is created
+                file.path = './uploads/'+file.name;
 
-    //set params
-    form.keepExtensions = true;
+                //HARDCODED FILE NAME FOR NOW, NEED TO TEST FOR HTML
+//                file.path = './uploads/data.html';
+            });
 
-    //register listener on file reception event..
-    //in order to change file path to one with file's original name.
-    //default generates random name for storage
-    form.on('fileBegin', function(name, file) {
-        //save to current directory
-        //need to specify file path at moment of file discovery
-        //otherwise it will be ignored when the writestream is created
-        file.path = './uploads/'+file.name;
-    });
+            //register this callback to progress event to display progress
+            form.on('progress', function(bytesReceived, bytesExpected) {
+                var percent_complete = (bytesReceived / bytesExpected) * 100;
+                console.log(percent_complete.toFixed(2));
+            });
 
-    //register this callback to progress event to display progress
-    form.on('progress', function(bytesReceived, bytesExpected) {
-        var percent_complete = (bytesReceived / bytesExpected) * 100;
-        console.log(percent_complete.toFixed(2));
-    });
+            form.on('end', function(){
+                var pythonChild = exec('python', ['python/userDataProc.py', './uploads/messages.htm']);
 
-    form.on('end', function(){
-        var pythonChild = exec('python', ['python/userDataProc.py', './uploads/messages.htm']);
+                pythonChild.stdout.on('data', function (data) {
+                    if (data.toString().indexOf('timeLine') != -1) {
 
-        pythonChild.stdout.on('data', function (data) {
-            if (data.toString().indexOf('timeLine') != -1) {
+                        fs.readFile('./wordcount.json', 'utf8', function (err, json) {
+                            if (err) {
+                                console.log(err);
+                            }
 
-                fs.readFile('./wordcount.json', 'utf8', function (err, json) {
-                    if (err) {
-                        console.log(err);
+                            dt.data = JSON.parse(json);
+                            console.log('dt.data has the json file')
+                        });
                     }
 
-                    dt.data = JSON.parse(json);
-                    console.log('dt.data has the json file')
+                    console.log('python out:  ' + data);
+                });
+
+                pythonChild.stderr.on('data', function (data) {
+                    console.log('python errout: ' + data);
+                });
+
+                pythonChild.on('close', function (code) {
+                    if (code != 0) {
+                        console.log('python closed with an error')
+                    }
+                });
+            });
+
+            //think of writing response in form.on('end',....);
+            form.parse(req, function(err, fields, files) {
+                res.writeHead(200, {'content-type': 'text/plain'});
+                res.write('received upload:\n\n');
+                res.end(util.inspect({fields: fields, files: files}));
+
+                //show file names
+                //console.log(files);
+            });
+
+            //read through the upload data after it's been loaded
+            function readStream(req){
+                var body = '';
+                req.on('data', function (data) {
+                    body += data;
+                    // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+
+                    if (body.length > 1e6) {
+                        //In case of flood, ends connection with 101
+                        req.connection.destroy();
+                    }
+                });
+                //when the request POST has finished uploading, output file
+                req.on('end', function () {
+                    //example of dumping to txt file
+                    var post = qs.parse(body);
+                    fs.writeFile("../uploads/stream.txt", post, function(err){
+                        if(err) {
+                            console.log(err);
+                        } else {
+                            console.log("The file was saved! Yay!");
+                        }
+                    });
+                    console.log(post);
+
+                });
+                req.on('error', function(e) {
+                  console.log('badness here because of: ' + e.message);
                 });
             }
 
-            console.log('python out:  ' + data);
-        });
+            return; //end transaction
 
-        pythonChild.stderr.on('data', function (data) {
-            console.log('python errout: ' + data);
-        });
+        }//end POST handler
 
-        pythonChild.on('close', function (code) {
-            if (code != 0) {
-                console.log('python closed with an error')
-            }
-        });
-    });
+        //if it's not a post, serve the file being requested
+        //need to check (mime) type of requested file. downloaded mime module to help
+        var uri = url.parse(req.url).pathname;
 
-    form.on('error', function(err){
-        console.log("File upload error: "+err);
-        res.send("File upload error: "+err, 500);
-    });
+        //if website is accessed by http://localhost:8000, fetch index (index.html)
+        if (uri == "/")
+        {
+            uri = "index.html";
+            //serve preloaded html
+            res.setHeader('content-type', mime.lookup(uri));
+            res.writeHead(200);
+            res.end(html);
 
-    //think of writing response in form.on('end',....);
-    form.parse(req, function(err, fields, files) {
-        res.writeHead(200, {'content-type': 'text/plain'});
-        res.write('received upload:\n\n');
-        res.end(util.inspect({fields: fields, files: files}));
-    });
+            //end transaction
+            return;
+        }
 
-    //read through the upload data after it's been loaded
-    function readStream(req){
-        var body = '';
-        req.on('data', function (data) {
-            body += data;
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-
-            if (body.length > 1e6) {
-                //In case of flood, ends connection with 101
-                req.connection.destroy();
-            }
-        });
-        //when the request POST has finished uploading, output file
-        req.on('end', function () {
-            //example of dumping to txt file
-            var post = qs.parse(body);
-            fs.writeFile("../uploads/stream.txt", post, function(err){
-                if(err) {
-                    console.log(err);
-                } else {
-                    console.log("The file was saved! Yay!");
+        //otherwise lookup file and server
+        //read file based on type
+        //currently one folder up
+        fs.readFile('./'+uri,
+            function (err, data)
+            {
+                if (err)
+                {
+                    res.writeHead(500);
+                    return res.end('Error loading index.html');
                 }
-            });
-            console.log(post);
 
-        });
-        req.on('error', function(e) {
-            console.log('badness here because of: ' + e.message);
+                //console.log(req.url);//shows action url
+                console.log('../'+uri);
+                res.setHeader('content-type', mime.lookup(uri));
+                res.writeHead(200);
+                res.end(data);
+            });
+
+    });
+
+    var io = socket(app);
+    app.listen(8000);
+
+    io.on('connection', function(socket){
+        socket.emit('server', dt.data);
+        console.log('sent the json');
+        /*
+        dt.on('data', function(){
+            console.log('when');
+            socket.emit('server', dataTransport.data);
+        });*/
+    });
+
+    function sendProcData(json) {
+        io.on('connection', function (socket) {
+            socket.emit('server', json);
         });
     }
+    console.log("Server listening on port 8000...");
 });
 
-var port = 8000;
-server = app.listen(port, console.log('Listening on port '+port+'...'));
-
-var io = socket.listen(server);
-
-io.on('connection', function(socket){
-    socket.emit('server', dt.data);
-    console.log(dt.data);
-    console.log('sent the json');
-});
-
-function sendProcData(json) {
-    io.on('connection', function (socket) {
-        socket.emit('server', json);
-    });
-}
